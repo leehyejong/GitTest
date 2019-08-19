1571f47de9694d00f11e01ad6cc96b450b049eac
PC-08 <PC-08@PC-08-PC>
2019-08-19 18:53:35 +0900

    세번째

--- src/kr/or/ddit/Hello.java
+++ src/kr/or/ddit/Hello.java
@@ -5,7 +5,8 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("hello, git");
        // 주석 추가ㄴ
-		String str = "git 배우고있어요"
+		String str ="git 배우고있어요";
+		//수정
 	}
 
 }
